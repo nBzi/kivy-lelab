e30027d7895eac4470b06310230e186e024479bc
nBzi <ric.spagnuolo@gmail.com>
2024-08-20 19:36:28 +0200

    lab v2 end

--- LeLabV2/main.py
+++ LeLabV2/main.py
@@ -6,16 +6,41 @@ from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 
 
-class WidgetsExamples(GridLayout):
+class WidgetsExemple(GridLayout):
     mon_texte = StringProperty("1")
+    # slider_value_txt = StringProperty("Valeur")
     compteur = 1
+    compteur_actif = BooleanProperty(False)
+    text_input_str = StringProperty("toto")
     def on_button_click(self):
-        print(self.compteur)
-        self.compteur += 1
-        self.mon_texte = str(self.compteur)
+        if self.compteur_actif:
+            print(self.compteur)
+            self.compteur += 1
+            self.mon_texte = str(self.compteur)
+
+    def on_toggle_button_state(self, widget):
+        if widget.state == "normal":
+            # print("OFF")
+            widget.text = "OFF"
+            self.compteur_actif = False
+        else:
+            # print("ON")
+            widget.text = "ON"
+            self.compteur_actif = True
+
+    def on_switch_active(self, widget):
+        print("Switch: " + str(widget.active))
+
+    # def on_slider_value(self, widget):
+    #     print(str(int(widget.value)))
+        # self.slider_value_txt = str(int(widget.value))
+
+    def on_text_validate(self, widget):
+        self.text_input_str = widget.text
+
 
 
 class MainWidget(Widget):
