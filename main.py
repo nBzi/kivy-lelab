45ba16d6e2368716de214bb93c4b0e0fdf520b79
nBzi <ric.spagnuolo@gmail.com>
2024-08-21 19:24:09 +0200

    update

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from navigation_screen_manager import NavigationScreenManager
+
+class MyScreenManager(NavigationScreenManager):
+    pass
+
+class LeLabApp(App):
+    manager = ObjectProperty(None)
+    def build(self):
+        self.manager = MyScreenManager()
+        return self.manager
+
+LeLabApp().run()
--- widget_exemples.py
+++ widget_exemples.py
@@ -1,12 +1,8 @@
-from kivy.app import App
-from kivy.metrics import dp
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.stacklayout import StackLayout
 from kivy.properties import StringProperty, BooleanProperty
+from kivy.lang import Builder
+
+Builder.load_file("widget_exemples.kv")
 
 
 class WidgetsExemple(GridLayout):
@@ -41,41 +37,3 @@ class WidgetsExemple(GridLayout):
     def on_text_validate(self, widget):
         self.text_input_str = widget.text
 
-
-
-class MainWidget(Widget):
-    pass
-
-
-class AnchorLayoutExemple(AnchorLayout):
-    pass
-
-
-class StackLayoutExemple(StackLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # self.orientation= "lr-bt"
-        for i in range(0, 100):
-            b = Button(text =str(i+1), size_hint=(None, None), size=(dp(100), dp(100)))
-            self.add_widget(b)
-
-
-
-class BoxLayoutExemple(BoxLayout):
-    pass
-"""    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-        b1 = Button(text="A")
-        b2 = Button(text="B")
-        b3 = Button(text="C")
-        self.add_widget(b1)
-        self.add_widget(b2)
-        self.add_widget(b3)"""
-
-
-class LeLabApp(App):
-    pass
-
-
-LeLabApp().run()
